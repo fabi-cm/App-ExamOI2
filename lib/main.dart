035aea4d1f6cfd57ad670905825615f2db32912f
fabi-cm <dahofcmmejia@gmail.com>
2025-05-06 19:27:04 -0400

    Conexion con la base de datos de Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'core/config.dart';
 import 'core/theme.dart';
 import 'core/router.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 
   // Limitar captura de errores de GPU en dispositivos problemáticos
   FlutterError.onError = (details) {
